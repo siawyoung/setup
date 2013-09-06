0971076a6af4e8afb8e89da12b5604c685718050
Ubuntu <ubuntu@ip-172-31-3-94.ap-southeast-1.compute.internal>
2013-09-06 08:34:58 +0000

    Deleted emacs install

--- setup.sh
+++ setup.sh
@@ -28,12 +28,6 @@ npm install -g jshint
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
 
-# Install emacs24
-# https://launchpad.net/~cassou/+archive/emacs
-sudo add-apt-repository -y ppa:cassou/emacs
-sudo apt-get -qq update
-sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
-
 # Install Heroku toolbelt
 # https://toolbelt.heroku.com/debian
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
